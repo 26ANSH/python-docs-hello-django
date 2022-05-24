ed036714fe1ecb54c01b5a2aac6c81dfa1134bbe
26ANSH <54389184+26ANSH@users.noreply.github.com>
2022-05-24 12:32:52 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
 def hello(request):
-    return HttpResponse("<h1>Hello, World!</h1><br>This is Ansh from Azure CLI")
+    if "USER_NAME" in os.environ:
+        return HttpResponse(f"<h1>Hello, World!</h1><br>This is Ansh from Azure CLI")
+    else:
+        return HttpResponse(f"<h1>Hello, {os.environ['USER_NAME']}!</h1><br>This is Ansh from Azure CLI")
+        
+        
+        
+        
